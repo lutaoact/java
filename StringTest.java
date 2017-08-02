1da6c02935bea460e2d17e99f08ac892f2021ae6
lutaoact <lutaoact@hotmail.com>
2017-08-02 11:52:42 +0800

    StringTest.java

---
+++ StringTest.java
@@ -0,0 +1,12 @@
+public class StringTest {
+  public static void main(String args[]) {
+      String s1 = "a";
+      String s2 = s1 + "b";
+      String s3 = "a" + "b";
+      System.out.println(s2 == "ab");
+      System.out.println(s3 == "ab");
+
+      String s = "a" + "b" + "c" + "d";
+      System.out.println(s == "abcd");
+  }
+}
